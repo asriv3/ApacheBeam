40a266ebcc4645133b667c2896837ae44d420f32
asriv3 <asriv3@gmail.com>
2022-07-17 18:16:49 +0100

    main changes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+
+options = PipelineOptions()
+p = beam.Pipeline(options=options)
 
 with beam.Pipeline as pipeline:
     pass
 
-from apache_beam.options.pipeline_options import PipelineOptions
+line = pipeline | 'ReadMyFile' >> beam.io.ReadFromText('/Users/Anurag/PycharmProjects/ApacheBeam/data/input/input_data.txt')
 
-beam_options = PipelineOptions()
